a2a4fe03e4ce1de629ba3f82039c0cea1b138112
Jesse <pokemonjm@gmail.com>
2025-01-01 13:47:33 -0500

    fixed env variable not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import apiKey from './config';
+const apiKey = import.meta.env.VITE_APP_API_KEY;
 import axios from 'axios';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
